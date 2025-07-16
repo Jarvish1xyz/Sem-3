6c5e378df4290f4e5d55b970de4e375a55d7be47
Sinha Milan <milans0415@gmail.com>
2025-07-16 05:35:25 +0000

    nodeJs

---
+++ WT/NodeJs/A1.js
@@ -0,0 +1,7 @@
+var path = require('path');
+console.log(path.dirname('/DS/A70.c'));
+console.log(path.basename('/DS/A70.c'));
+console.log(path.join('/DS','/A70.c'));
+console.log(path.extname('/DS/A70.c'));
+console.log(path.relative('/WT/NodeJs','/WT/abc'));
+console.log(path.resolve('/WT/abc','A1.js'));
