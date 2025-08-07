f73054a3e6915c9efa2461c6d5e2b1829566406c
Sinha Milan <milans0415@gmail.com>
2025-08-07 14:46:22 +0000

    Carpooling

---
+++ ProjectCar/newProject/carpooling/src/components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+    return (
+        <>
+            <div>
+                <div className="h2 text-white text-center bg-black ">
+                    Hello world!!!
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
